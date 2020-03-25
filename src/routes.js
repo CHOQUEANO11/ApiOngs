6244173912a0c3a6da6bf73ae7a4b6d92944b9e8
cmtgeralpmpa <pmpa123456>
2020-03-25 18:32:49 -0300

    primary commit

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const OngController = require('./controller/OngController');
+const IncidentsContoller = require('./controller/IncidentsController');
+const ProfileController = require('./controller/ProfileController');
+const SessionController = require('./controller/SessionController')
+
+const routes = express.Router();
+
+routes.post('/sessions', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentsContoller.index);
+routes.post('/incidents', IncidentsContoller.create);
+routes.delete('/incidents/:id', IncidentsContoller.delete);
+
+
+
+module.exports = routes;
+
+
+
